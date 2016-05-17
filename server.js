252587a13f227cc146f0d7034ce00b2db9014a44
Teancum Besendorfer <teancum.besendorfer@gmail.com>
2016-05-16 23:42:44 -0600

    Add the main .js file

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Reversi = require('./Reversi/Reversi.js') || {};
+var BoardDimensionError = require('./Reversi/Error/BoardDimensionError.js') || {};
+
+try {
+	Reversi = new Reversi(10);
+
+	Reversi.run();
+}
+catch (e) {
+	if (e instanceof BoardDimensionError)
+		e.printError();
+}
